45b21ddbe81f8066d553808fcd95376f5191c200
GOPINATHSF4767 <gopinath.natesan@syncfusion.com>
2026-07-14 14:52:49 +0530

    Added sample for DOCX-to-MD

---
+++ Performance-metrices/DOCX-to-MD/.NET/DOCX-to-MD-Conversion/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Diagnostics;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+
+
+class Program
+{
+    static void Main()
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        //Open a file as a stream.
+        using (FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"../../../Data/Input.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+        {
+            //Load the file stream into a Word document.
+            using (WordDocument document = new WordDocument(fileStreamPath, FormatType.Docx))
+            {
+                //Create a file stream.
+                using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Output/Result.md"), FileMode.Create, FileAccess.ReadWrite))
+                {
+                    //Save a Markdown file to the file stream.
+                    document.Save(outputFileStream, FormatType.Markdown);
+                    stopwatch.Stop();
+                    Console.WriteLine($"Time taken to open and save a 100-page document as Markdown: {stopwatch.Elapsed.TotalSeconds} seconds");
+                }
+            }
+        }
+    }
+}
