3600803f512715452707e700998a6be6504040fe
GOPINATHSF4767 <gopinath.natesan@syncfusion.com>
2026-07-14 14:57:04 +0530

    Added sample for PDF to MD

---
+++ Performance-metrices/PDF-to-MD/.NET/PDF-to-MD-Conversion/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Diagnostics;
+using System.Text;
+using Syncfusion.SmartDataExtractor;
+
+class Program
+{
+    static void Main()
+    {   
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        //Open the input PDF file as a stream.
+        using (FileStream stream = new FileStream("../../../Data/Document-50.pdf", FileMode.Open, FileAccess.Read))
+        {
+            //Initialize the Data Extractor.
+            DataExtractor extractor = new DataExtractor();
+            //Extract data as Markdown.
+            string data = extractor.ExtractDataAsMarkdown(stream);
+            //Save the extracted Markdown data into an output file.
+            File.WriteAllText("../../../Output/Result.md", data, Encoding.UTF8);
+        }
+        stopwatch.Stop();
+        Console.WriteLine($"Time taken to save PDF document as Markdown: {stopwatch.Elapsed.TotalSeconds} seconds");
+        Console.WriteLine($"After: {GetMemoryUsageMB()} MB");
+    }
+}
