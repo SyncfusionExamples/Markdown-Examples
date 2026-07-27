3032fe6b0c7b283664d294a61fcd4ee7ba445753
GOPINATHSF4767 <gopinath.natesan@syncfusion.com>
2026-07-27 19:50:27 +0530

    Added samples

---
+++ HTML-conversions/Convert-Markdown-to-HTML/.NET/Convert-Markdown-to-HTML/Program.cs
@@ -0,0 +1,19 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.IO;
+
+namespace Convert_Markdown_to_HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Open an existing Markdown file.
+            using (WordDocument document = new WordDocument(Path.GetFullPath("Data/Input.md")))
+            {
+                //Save as a HTML document.
+                document.Save(Path.GetFullPath("Output/Output.html"));
+            }
+        }
+    }
+}
