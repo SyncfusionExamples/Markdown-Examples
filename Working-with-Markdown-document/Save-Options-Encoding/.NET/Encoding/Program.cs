3eb3b8d14a96dc51d3f1d560bf212bae2c062f06
GOPINATHSF4767 <gopinath.natesan@syncfusion.com>
2026-07-28 13:29:08 +0530

    Added samples

---
+++ Working-with-Markdown-document/Save-Options-Encoding/.NET/Encoding/Program.cs
@@ -0,0 +1,23 @@
+﻿using Syncfusion.Office.Markdown;
+using System.Text;
+
+
+namespace Encoding_in_SaveOptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Opens an existing Markdown document.
+            MarkdownDocument markdownDocument = new MarkdownDocument("Data/Input.md");
+            // Creates a SaveOptions instance.
+            SaveOptions saveOptions = new SaveOptions();
+            // Sets the encoding to use when saving the Markdown document.
+            saveOptions.Encoding = Encoding.Unicode;
+            // Saves the Markdown document with the specified encoding.
+            markdownDocument.Save("Output/Output.md", saveOptions);
+            // Disposes the document to release all memory.
+            markdownDocument.Dispose();
+        }
+    }
+}
